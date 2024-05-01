e775961ef98d9cf70da84e22b58450aedd776fd9
aliasghar <aliasghar.salimi.05@gmail.com>
2024-05-01 17:30:23 +0330

    published and drafting antion created for the admin panel section

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,36 @@
 from django.contrib import admin
 from .models import articles, Category
 
+# admin.site.disable_action("delete_selected") # this code is used to disabe or delete an action
+
 # Register your models here.
+
+
+def make_published(modeladmin, request, queryset):
+    updated = queryset.update(status='p')
+    if updated == 1:
+        message_bit = 'منتشر شد'
+    elif updated > 1:
+        message_bit = 'منتشر شدند'
+    modeladmin.message_user(request, f"{updated} مقاله {message_bit}")
+
+
+def make_draft(modeladmin, request, queryset):
+    updated = queryset.update(status='d')
+    if updated == 1:
+        message_bit = 'پیشنویس شد'
+    elif updated > 1:
+        message_bit = 'پیشنویس شدند'
+    modeladmin.message_user(request, f"{updated} مقاله {message_bit}")
+make_published.short_description = "انتشار مقالات انتخاب شده"
+make_draft.short_description = "پیشنویس کردن مقالات انتخاب شده"
+
 class articleAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'jpublish', 'status', 'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug' : ('title',)}
+    actions = [make_published, make_draft]
 
     def category_to_str(self, obj):
         return "، ".join([cat.title for cat in obj.category_published()])
@@ -19,5 +43,6 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ( 'title', 'slug','parent', 'position', 'status')
     list_filter = (['status'])
     search_fields = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Category, CategoryAdmin)
